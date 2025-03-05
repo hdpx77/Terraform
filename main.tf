088e6e2f7cc247d8a38c51e953c23e1286f8cd8c
hdpx77 <hemanthdibburprakash@gmail.com>
2025-03-05 14:17:14 +0000

    [Update main.tf]: configure Route Table & Associate with Public Subnets

--- main.tf
+++ main.tf
@@ -25,3 +25,19 @@ resource "aws_subnet" "private" {
   cidr_block       = "10.0.${count.index + 10}.0/24"
   availability_zone = element(["us-east-1a", "us-east-1b", "us-east-1c"], count.index)
 }
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_route" "internet_access" {
+  route_table_id         = aws_route_table.public.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.gw.id
+}
+
+resource "aws_route_table_association" "public" {
+  count          = 3
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
+}
+
