fb5e81e2d115819cac3dd69aba64409e85ce7c52
hdpx77 <hemanthdibburprakash@gmail.com>
2025-03-05 14:15:39 +0000

    [Update main.tf]: Configure private and Public subnets

--- main.tf
+++ main.tf
@@ -11,3 +11,17 @@ resource "aws_vpc" "main" {
 resource "aws_internet_gateway" "gw" {
   vpc_id = aws_vpc.main.id
 }
+resource "aws_subnet" "public" {
+  count             = 3
+  vpc_id           = aws_vpc.main.id
+  cidr_block       = "10.0.${count.index}.0/24"
+  availability_zone = element(["us-east-1a", "us-east-1b", "us-east-1c"], count.index)
+  map_public_ip_on_launch = true
+}
+
+resource "aws_subnet" "private" {
+  count             = 3
+  vpc_id           = aws_vpc.main.id
+  cidr_block       = "10.0.${count.index + 10}.0/24"
+  availability_zone = element(["us-east-1a", "us-east-1b", "us-east-1c"], count.index)
+}
