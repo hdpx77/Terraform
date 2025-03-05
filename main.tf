5134eb540df76a79d842684880d433fb2155b683
hdpx77 <hemanthdibburprakash@gmail.com>
2025-03-05 14:30:11 +0000

    [Update main.tf]: Update to parameterized dynamic values

--- main.tf
+++ main.tf
@@ -1,45 +1,29 @@
 provider "aws" {
-  region = "us-east-1"  
-}
-variable "vpc_cidr" {
-  default = "10.0.0.0/16"
+  region = "var.region"  
 }
 
 resource "aws_vpc" "main" {
   cidr_block = var.vpc_cidr
 }
-resource "aws_internet_gateway" "gw" {
-  vpc_id = aws_vpc.main.id
-}
+
 resource "aws_subnet" "public" {
-  count             = 3
+ count             = length(var.public_subnets)
   vpc_id           = aws_vpc.main.id
-  cidr_block       = "10.0.${count.index}.0/24"
+  cidr_block       = var.public_subnets[count.index]
   availability_zone = element(["us-east-1a", "us-east-1b", "us-east-1c"], count.index)
   map_public_ip_on_launch = true
 }
 
 resource "aws_subnet" "private" {
-  count             = 3
+  count             = length(var.private_subnets)
   vpc_id           = aws_vpc.main.id
-  cidr_block       = "10.0.${count.index + 10}.0/24"
+  cidr_block       = var.private_subnets[count.index]
   availability_zone = element(["us-east-1a", "us-east-1b", "us-east-1c"], count.index)
 }
 resource "aws_route_table" "public" {
   vpc_id = aws_vpc.main.id
 }
 
-resource "aws_route" "internet_access" {
-  route_table_id         = aws_route_table.public.id
-  destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = aws_internet_gateway.gw.id
-}
-
-resource "aws_route_table_association" "public" {
-  count          = 3
-  subnet_id      = aws_subnet.public[count.index].id
-  route_table_id = aws_route_table.public.id
-}
 resource "aws_security_group" "web" {
   vpc_id = aws_vpc.main.id
 
@@ -47,9 +31,14 @@ resource "aws_security_group" "web" {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = [var.http_ingress_cidr]
   }
-
+ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [var.ssh_ingress_cidr]
+  }
   egress {
     from_port   = 0
     to_port     = 0
